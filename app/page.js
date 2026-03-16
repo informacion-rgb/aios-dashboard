3ca72dc2a13142c26b7ed9b23d71509c256cf6c2
informacion-rgb <informacion@suarezia.com>
2026-03-16 09:24:42 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
-
-import { getServerSession } from "next-auth"
-import { redirect } from "next/navigation"
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import { authOptions } from "../lib/auth";
 
 async function getApiStatus() {
